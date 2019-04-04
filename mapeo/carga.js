45004768d3ff907c88d81f701cf71e94b7356b10
Pedro Blanco Wasmer <pedro.blanco.wasmer@juntadeandalucia.es>
2019-04-04 13:17:18 +0200

    Incorporación de las fichas de los demás compañeros/as

--- mapeo/carga.js
+++ mapeo/carga.js
@@ -8,15 +8,69 @@ $( window ).load(function() {
 		procesar_json ( result );
 	});
 	*/
-	
+	/*
 	var cadena_json = '{"0":{"0":"NOMBREs PARTICIPANTE","1":"ID. Participante","2":"CODIGO ficha","3":"T\u00cdTULO de la iniciativa","4":"ENTIDAD promotora","5":"PERSONAS de contacto (Datos)","6":"A\u00f1o DESDE","7":"A\u00d1O HASTA","8":"\u00bfIMPLEMENTADA? (SI o NO)","9":"ORIGEN (EM o INST)","10":"PROVINCIA IMPULSORA","11":"PROVINCIAS APLICACI\u00d3N","12":"T.I: 1-SERVICIOS","13":"T.I. 2-PROCESOS","14":"T.I. 3-GESTION PERSONAS","15":"T.I. 4-PARTICIPACI\u00d3N","16":"T.I. 5-ORGANIZATIVA","17":"T.I. 6-CAMBIO CULTURAL","18":"T.I. 7-TECNOL\u00d3GICA","19":"T.I. 8-TRANSPARENCIA","20":"T.I. 9-COLABORACI\u00d3N","21":"T.I. 10-GEST.CON","22":"T.I. 11-BIENESTAR","23":"T.I. 12-IGUALDAD GEN","24":"T.I. 13-SENSIB.CUIDAD.","25":"T.I. 14-NORMATIVO","26":"A.A. 1-TURISMO","27":"A.A. 2-JUSTICIA","28":"A.A. 3-ADMON PUB.","29":"A.A. 4-INTERIOR","30":"A.A. 5-EMPLEO","31":"A.A. 6-HACIENDA","32":"A.A. 7-IND.ENERG","33":"A.A. 8-EDUCACI\u00d3N","34":"A.A. 9-DEPORTE","35":"A.A. 10-AGRIC-GAN-PESC","36":"A.A. 11-M.AMBIENTE","37":"A.A. 12-ECON.EMP.","38":"A.A. 13-CON-UNIV","39":"A.A. 14-SALUD","40":"A.A. 15-POLIT.SOC.","41":"A.A. 16-MEDIO RURAL","42":"A.A. 17-MOVILIDAD","43":"A.A. 18-CULTURA","44":"A.A. 19-FOMENTO","45":"Ayuntamientos","46":"Diputaciones"},"1":{"0":"Pedro Blanco Wasmer","1":"2","2":"2.1","3":"Observatorio de Pr\u00e1cticas Innovadoras en el Manejo de Enfermedades Cr\u00f3nicas Complejas (OPIMEC)","4":"Secretar\u00eda General de Salud P\u00fablica y Consumo de la Consejer\u00eda de Salud de la Junta de Andaluc\u00eda","5":"Carmen P\u00e9rez Romero","6":"2006","7":"Actualidad","8":"SI","9":"INST","10":"ANDALUCIA","11":"ANDALUCIA","12":"","13":"","14":"","15":"","16":"X","17":"X","18":"","19":"","20":"X","21":"X","22":"","23":"","24":"","25":"","26":"","27":"","28":"","29":"","30":"","31":"","32":"","33":"","34":"","35":"","36":"","37":"","38":"X","39":"X","40":"","41":"","42":"","43":"","44":"","45":"","46":""},"2":{"0":"Pedro Blanco Wasmer","1":"2","2":"2.2","3":"Escuela de pacientes","4":"Escuela Andaluza de Salud P\u00fablica (EASP) - Consejer\u00eda de Salud y Familias de la Junta de Andaluc\u00eda","5":"escueladepacientes.sspa@juntadeandalucia.es","6":"2008","7":"Actualidad","8":"SI","9":"INST","10":"ANDALUCIA","11":"ANDALUCIA","12":"X","13":"X","14":"","15":"X","16":"X","17":"X","18":"","19":"","20":"X","21":"X","22":"","23":"","24":"X","25":"","26":"","27":"","28":"","29":"","30":"","31":"","32":"","33":"","34":"","35":"","36":"","37":"","38":"","39":"X","40":"X","41":"","42":"","43":"","44":"","45":"X","46":""},"3":{"0":"Pedro Blanco Wasmer","1":"2","2":"2.3","3":"Proyecto Genoma M\u00e9dico","4":"Consejer\u00eda de Salud y Familias de la Junta de Andaluc\u00eda","5":"Javier Santoyo L\u00f3pez","6":"2009","7":"Actualidad","8":"SI","9":"INST","10":"ANDALUCIA","11":"SEVILLA","12":"X","13":"","14":"","15":"","16":"","17":"","18":"X","19":"","20":"","21":"X","22":"","23":"","24":"","25":"","26":"","27":"","28":"","29":"","30":"","31":"","32":"","33":"","34":"","35":"","36":"","37":"","38":"X","39":"X","40":"","41":"","42":"","43":"","44":"","45":"","46":""},"4":{"0":"Pedro Blanco Wasmer","1":"2","2":"2.4","3":"@firma: La plataforma corporativa de autenticaci\u00f3n y firma","4":"D.G. de Transformaci\u00f3n Digital de la Consejer\u00eda de Hacienda, Industria y Energ\u00eda de la Junta de Andaluc\u00eda","5":"D.G. de Transformaci\u00f3n Digital de la Consejer\u00eda de Hacienda, Industria y Energ\u00eda de la Junta de Andaluc\u00eda","6":"2003","7":"Actualidad","8":"SI","9":"INST","10":"ANDALUCIA","11":"ANDALUCIA","12":"X","13":"X","14":"","15":"","16":"X","17":"","18":"X","19":"","20":"","21":"","22":"","23":"","24":"","25":"X","26":"","27":"","28":"X","29":"","30":"","31":"","32":"","33":"","34":"","35":"","36":"","37":"","38":"","39":"","40":"","41":"","42":"","43":"","44":"","45":"X","46":"X"},"5":{"0":"Pedro Blanco Wasmer","1":"2","2":"2.6","3":"Tarjeta +Cuidado, tarjeta de Identificaci\u00f3n personal para el Plan de Atenci\u00f3n a Personas Cuidadoras","4":"PiCuida - Estrategia de Cuidados de Andaluc\u00eda, Consejer\u00eda de Salud y Familias de la Junta de Andaluc\u00eda","5":"https:\/\/www.picuida.es\/ciudadania\/contacto\/","6":"2007","7":"Actualidad","8":"SI","9":"INST","10":"ANDALUCIA","11":"ANDALUCIA","12":"X","13":"","14":"","15":"X","16":"X","17":"","18":"","19":"","20":"","21":"","22":"","23":"X","24":"X","25":"","26":"","27":"","28":"","29":"","30":"","31":"","32":"","33":"","34":"","35":"","36":"","37":"","38":"","39":"X","40":"X","41":"","42":"","43":"","44":"","45":"","46":""},"6":{"0":"Pedro Blanco Wasmer","1":"2","2":"2.7","3":"Proyecto \u201cSobre Ruedas\u201d","4":"Secretar\u00eda General de Salud P\u00fablica y Consumo de la Consejer\u00eda de Salud (y Familias, actualmente) de la Junta de Andaluc\u00eda","5":"Secretar\u00eda General de Salud P\u00fablica y Consumo de la Consejer\u00eda de Salud (y Familias, actualmente) de la Junta de Andaluc\u00eda","6":"2009","7":"Actualidad","8":"SI","9":"INST","10":"ANDALUCIA","11":"ANDALUCIA","12":"X","13":"","14":"","15":"X","16":"","17":"","18":"X","19":"","20":"X","21":"","22":"","23":"","24":"X","25":"","26":"","27":"","28":"","29":"","30":"","31":"","32":"","33":"X","34":"","35":"","36":"","37":"","38":"","39":"X","40":"","41":"","42":"X","43":"","44":"","45":"X","46":""}}';
 	
 	procesar_json ( cadena_json );
+	*/
+	
+	$.ajax({
+		  url: "datos.json",
+		  context: document.body,
+		  async: false,
+		  datatype: "json"
+		}).done(function(resultado) {
+			console.log ( 'Procesando fichero principal: datos.json' );
+			console.log (resultado);
+
+//			var lista_ficheros = JSON.parse ( resultado );
+			var lista_ficheros = resultado;
+			var json_completo = Array ();
+
+			for ( i = 0 ; i < lista_ficheros.length ; i++ ) {
+				var n_objeto = procesar_fichero_json ( lista_ficheros[i] )
+				console.log ( 'Push:' );
+				console.log ( n_objeto );
+				if ( i == 0 ) {
+					json_completo.push ( n_objeto );
+				} else {
+					for ( fila in n_objeto ) {
+						console.log ( n_objeto[fila] );
+						json_completo.push ( n_objeto[fila] );
+					}
+				}
+			}
+			
+			console.log ( json_completo );
+			
+			procesar_json ( json_completo );
+		});
+
+	
 });
 
-function procesar_json ( json )
+
+function procesar_fichero_json ( fichero_json )
 {
-	var resultado = JSON.parse ( json );
+	var objeto_devuelto;
+	$.ajax({
+		  url: fichero_json,
+		  context: document.body,
+		  async: false,
+		  datatype: "json"
+		}).done(function(resultado) {
+			console.log ( 'Procesando fichero: '+fichero_json );
+			console.log (resultado );
+			objeto_devuelto = resultado;
+		});	
+	return objeto_devuelto;
+}
+
+
+function procesar_json ( resultado )
+//function procesar_json ( json )
+{
+//	var resultado = JSON.parse ( json );
 	console.log ( 'Cargadas ' + ( Object.keys(resultado).length - 1) + ' iniciativas' );
 	
 	var cabecera = '<thead>';
@@ -25,8 +79,10 @@ function procesar_json ( json )
 	cabecera += '<th data-priority="3">'+resultado['0']['4']+'</th>';
 	cabecera += '</thead>';
 	$('#resultados').append ( cabecera );
+//	$('#resultados').prepend ( cabecera );
 	
 	var cuerpo = '<tbody>';
+//	var cuerpo = '';
 	var total = Object.keys(resultado).length;
 	for ( var i = 1 ; i < total ; i++ ) {
 		var indice = "'"+i+"'";
@@ -43,6 +99,8 @@ function procesar_json ( json )
 	}
 	cuerpo += '</tbody>';
 	$('#resultados').append ( cuerpo );
+	
+	//$('table#resultados').table( "refresh" ).trigger( "create" );
 	
 	$('#searchFilterReveal').next('.ui-textinput-clear-button').click();
 }
--- mapeo/importar_csv.php
+++ mapeo/importar_csv.php
@@ -1,28 +1,56 @@
 <?php
 
-$nombre_fichero = '2019-04-03.csv';
+$prefijo_fichero = '2019-04-04';
 
-$resultado_csv = array ();
+$fichero_cabecera = 'cabecera.json';
+$cabecera_procesada = false;
 
-$resultado_json = '{"0":';
+$lista_ficheros_json = 'datos.json';
 
-// TODO: Tal vez haya que hacer un par de comprobaciones
-$fichero_csv = fopen ( $nombre_fichero, 'r' );
+$lista_json = Array();
 
-//$fila_csv = fgetcsv($fichero_csv, 0, ',', '"', '\\');
-for ( $i = 0 ; $fila_csv = fgetcsv($fichero_csv, 0, ',', '"', '\\' ) ; $i++, $resultado_json .= ',"'.$i.'":' )
-{
+$array_ficheros = glob ( $prefijo_fichero.'-*.csv' );
+
+foreach ( $array_ficheros as $fichero) {
+    $fichero_json = substr ( $fichero, 0, -4 ).'.json';
+    
+    
+    $resultado_csv = array ();
+    
+    $resultado_json = '{"0":';
+    
+    // TODO: Tal vez haya que hacer un par de comprobaciones
+    $fichero_csv = fopen ( $fichero, 'r' );
+    
+    // Si no hemos incluido un fichero con la cabecera, lo creamos extrayendo la cabecera del primer csv
+    // Si lo hemos incluido descartamos la cabecera del csv (no hacemos nada con ella)
+    $fila_csv = fgetcsv($fichero_csv, 0, ',', '"', '\\');
+    if ( ! $cabecera_procesada ) {
+        $cabecera_procesada = true;
+        file_put_contents( $fichero_cabecera , json_encode ( $fila_csv, JSON_FORCE_OBJECT ) );
+        echo $fichero_cabecera.'<br/>';
+        $lista_json[] = $fichero_cabecera;
+    }
+
+    echo $fichero_json.'<br/>';
+    
+    for ( $i = 0 ; $fila_csv = fgetcsv($fichero_csv, 0, ',', '"', '\\' ) ; $i++, $resultado_json .= ',"'.$i.'":' )
+    {
         $resultado_csv[] = $fila_csv;
-//        print_r ( $fila_csv );
-//        print ( '<br/>' );
+        //        print_r ( $fila_csv );
+        //        print ( '<br/>' );
         $resultado_json .= json_encode ( $fila_csv, JSON_FORCE_OBJECT );
+    }
+    
+    fclose($fichero_csv);
+    
+    $resultado_json = substr_replace( $resultado_json, '', - strlen(',"'.$i.'":' ));
+    
+    $resultado_json .= '}';
+    
+    file_put_contents( $fichero_json , $resultado_json );
+    
+    $lista_json[] = $fichero_json;
 }
 
-fclose($fichero_csv);
-
-$resultado_json = substr_replace( $resultado_json, '', - strlen(',"'.$i.'":' ));
-
-$resultado_json .= '}';
-
-print ( $resultado_json );
-
+file_put_contents( $lista_ficheros_json , json_encode ( $lista_json ) );
