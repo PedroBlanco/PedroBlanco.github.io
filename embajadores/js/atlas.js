9789bf43dd44132b9efe9b7c24a807b5e646352b
Pedro Blanco Wasmer <9304344+PedroBlanco@users.noreply.github.com>
2022-06-16 12:32:52 +0200

    feat: Actualizo Atlas de Embajadores

--- embajadores/js/atlas.js
+++ embajadores/js/atlas.js
@@ -43,7 +43,12 @@ function init_map_options ( _zoom ) {
         minZoom: 5,
         zoomDelta: 0.5,
         zoomSnap: 0.5,
-        zooms: [5, 5.5, 6, 6.5, 7, 7.5, 8, 8.5, 9, 9.5, 10, 10.5, 11, 11.5, 12, 12.5, 13, 13.5, 14, 14.5, 15, 15.5, 16, 16.5, 17]
+        zooms: [5, 5.5, 6, 6.5, 7, 7.5, 8, 8.5, 9, 9.5, 10, 10.5, 11, 11.5, 12, 12.5, 13, 13.5, 14, 14.5, 15, 15.5, 16, 16.5, 17],
+        // Opciones para L.control.layers.tree
+        selectorBack: false,
+        closedSymbol: '&#8862; &#x1f5c0;',
+        openedSymbol: '&#8863; &#x1f5c1;',
+        collapsed: false,
       };
 }
 
@@ -86,17 +91,13 @@ function init_icon_oil_drop ( ) {
 
 // TODO: Configurar nuevo marcador para los embajadores
 function init_icon_ambassador ( ) {
-    var _icon = 'images/marker-oil-drop-fit.png';
-    var _shadow = 'images/marker-oil-drop-fit-shadow.png';
+    var _icon = 'images/marker_km_32.png';
 
     return L.icon ({
         iconUrl: _icon,
-        iconSize: [25,41],
-        iconAnchor: [12,39],
-        shadowUrl: _shadow,
-        shadowSize: [41,41],
-        shadowAnchor: [30,37],
-        popupAnchor: [1,-33]
+        iconSize: [40,40],
+        iconAnchor: [20,16],
+        popupAnchor: [0,-14]
     });
 }
 
